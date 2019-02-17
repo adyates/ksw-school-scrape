428ab09ecc48eef0f265a362a63dd4e103fa67cb
Alvin <mambothief@gmail.com>
2019-02-17 12:11:49 -0800

    Adjust geocoding to handle more than US addresses

--- geocoder_googs.py
+++ geocoder_googs.py
@@ -7,73 +7,52 @@ When saving to GCS, uses the same folder/file path as fetch.py
 Uses the Google Maps Geocoding API.
 """
 
-import csv
 import io
 import json
 import os
 
-from google.cloud import storage
 import googlemaps
+import pandas as pd
 
-import fetch
+import fetch_wksa as fetch
 import gcs
 
 
 SCHOOL_GEODATA_FILE = 'data/school_geodata.csv'
 
 
-def exportGeoData(school_list, file_name=None):
-
+def exportGeoData(school_df, file_name=None):
+    """Write the geodata file from the dataframe, either to GCS or locally."""
     file_out = io.StringIO() if file_name else open(SCHOOL_GEODATA_FILE, 'w')
 
-    with file_out as csvout:
-        writer = csv.DictWriter(
-            csvout, [
-                # Original fields from the scrape
-                'City', 'Region', 'Address', 'Phone #s', 'Instructor',
-                # Google Maps additional data
-                'lat', 'lon', 'type', 'formatted_address'
-            ], lineterminator='\n')
+    school_df.to_csv(file_out)
 
-        header = {
-            'Region': 'Region',
-            'City': 'City',
-            'Address': 'Address',
-            'Phone #s':'Phone #s',
-            'Instructor': 'Instructor',
-            'lat': 'Latitude',
-            'lon': 'Longitude',
-            'type': 'Geocode Type',
-            'formatted_address': 'Google Address'
-        }
-
-        writer.writerow(header)
-        for school in school_list:
-            writer.writerow(school)
-
-        if file_name:
-            # Save to GCS
-            blob = gcs.getGeocodeBucket().blob('%s' % file_name)
-            blob.upload_from_string(file_out.getvalue(), content_type='text/csv')
+    if file_name:
+        # Save to GCS
+        blob = gcs.getGeocodeBucket().blob('%s' % file_name)
+        blob.upload_from_string(file_out.getvalue(), content_type='text/csv')
 
 class LimitedApiManager:
-
+    """Abstraction around any geocoding APIs that this file may need."""
     def __init__(self, cps):
         self.client = googlemaps.Client(
             key=os.environ['GOOGLE_GEOCODE_API_KEY'],
             queries_per_second=cps,
         )
 
-    def get(self, address):
-        print('  Fetching %s' % address)
-        results = self.client.geocode(address)
+    def get(self, address, country):
+        """Get the geocoding results for a given address."""
+        # Append the country to make isolate the search to the correct areas
+        search_address = ', '.join([address, country])
 
-        if len(results) != 1:
-            print('  Found %s results for: %s' % (len(results), address))
-            return None
-        return results[0]
+        print('  Fetching %s' % search_address)
+        results = self.client.geocode(search_address)
+        print('  Found %s results for: %s' % (len(results), search_address))
+        return results
+
 
 def _loadGCSDataFile(file_name):
+    """Get a file handle for a file from GCS."""
     file_stream = io.StringIO(
         gcs.getFetchBucket().blob(file_name).download_as_string().decode(),
         newline=''
@@ -82,37 +61,64 @@ def _loadGCSDataFile(file_name):
 
 
 def loadSchoolData(file_name=None):
+    """Load school data from a file and process it."""
     data_file = _loadGCSDataFile(file_name) if file_name else open(fetch.SCHOOL_EXPORT_FILE, 'r')
     geocode_api = LimitedApiManager(30)
 
-    school_data = csv.DictReader(data_file)
+    school_df = pd.read_csv(data_file, keep_default_na=False)
     school_list = []  # Save each row for later re-write
-    for item in school_data:
-        def _handleResponse(result):
-            if not result:
-                return None
-            return result
+    for row_tuple in school_df.iterrows():
+        def _handleResponse(results, assure_address=True):
+            """Process the results"""
+            if len(results) != 1:
+                if assure_address:
+                    return None
+                # Filter out only the approximate addresses
+                results = [result for result in results if (
+                    result['geometry']['location_type'] == 'APPROXIMATE'
+                )]
+            return results[0]
 
         # Fetch the address first.  If it fails, switch to city+state
+        geocode_data = {
+            'lat': '',
+            'lon': '',
+            'type': '',
+            'formatted_address': '',
+        }
+        item = row_tuple[1]
         print('Processing %s' % item['Address'])
         geodata = (
-            item['Address'] and _handleResponse(geocode_api.get(address=item['Address'])) or
-            _handleResponse(geocode_api.get(
-                address=('%s, %s' % (item['City'], item['Region'])))))
+            item['Address'] and _handleResponse(
+                geocode_api.get(address=item['Address'], country=item['Country'])
+            ) or
+            _handleResponse(
+                geocode_api.get(
+                    address=('%s, %s' % (item['City'], item['Region'])),
+                    country=item['Country']
+                ),
+                assure_address=False
+            ))
         if not geodata:
             print('  Unable to find geodata for:\n%s' % json.dumps(item, indent=2))
             continue
         geometry = geodata['geometry']
 
-        item.update({
+        geocode_data.update({
             'lat': geometry['location']['lat'],
             'lon': geometry['location']['lng'],
             'type': geometry['location_type'],
             'formatted_address': geodata['formatted_address'],
         })
-        school_list.append(item)
+        school_list.append(geocode_data)
 
-    exportGeoData(school_list, file_name)
+    # Create the new CSV columns in the dataframe
+    school_df['Latitude'] = pd.Series([school['lat'] for school in school_list])
+    school_df['Longitude'] = pd.Series([school['lon'] for school in school_list])
+    school_df['Geocode Type'] = pd.Series([school['type'] for school in school_list])
+    school_df['Google Address'] = pd.Series([school['formatted_address'] for school in school_list])
+
+    exportGeoData(school_df, file_name)
     data_file.close()
 
 
