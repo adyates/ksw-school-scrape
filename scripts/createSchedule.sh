f84391b5a0bf7e3480ddb494d739d385f59296fa
Alvin <mambothief@gmail.com>
2019-10-18 15:06:00 -0500

    Double the frequency of scraping

--- scripts/createSchedule.sh
+++ scripts/createSchedule.sh
@@ -3,6 +3,6 @@
 # Publish to hoh-gwuhn-scrape at the 1st of every month for UTC Noon
 gcloud beta scheduler jobs delete monthly-scrape
 gcloud beta scheduler jobs create pubsub monthly-scrape --topic="hoh-gwuhn-scrape" \
-  --schedule="0 12 1 * *" --message-body="{}" \
-  --description="Publish to hoh-gwuhn-scrape at the 1st of every month for UTC Noon" \
+  --schedule="0 12 1,15 * *" --message-body="{}" \
+  --description="Publish to hoh-gwuhn-scrape on the 1st and 15th of every month for UTC Noon" \
   --max-retry-attempts=5 --min-backoff="1h" --max-backoff="1d"
